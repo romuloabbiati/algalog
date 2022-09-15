b494fc835c69c8952b40a904339be5546ff28508
Romulo <rhabbiati@hotmail.com>
2022-09-15 05:24:10 +0100

    Spring Data JPA config

--- src/main/java/com/algaworks/algalog/api/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClientController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Client;
+import com.algaworks.algalog.domain.repository.ClientRepository;
 
 @RestController
 public class ClientController {
@@ -16,9 +18,12 @@ public class ClientController {
 	@PersistenceContext
 	private EntityManager manager;
 	
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	@GetMapping("/clients")
 	public List<Client> listar() {
-		return manager.createQuery("from Client", Client.class).getResultList();
+		return clientRepository.findAll();
 	}
 	
 }
