bc8bf3a68c9326e800ef4cb0b103d5177af00cc5
Romulo <rhabbiati@hotmail.com>
2022-09-15 05:04:46 +0100

    Jakarta Persistence JPA config

--- src/main/java/com/algaworks/algalog/api/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClientController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,12 @@ import com.algaworks.algalog.domain.model.Client;
 @RestController
 public class ClientController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clients")
 	public List<Client> listar() {
-		var client1 = new Client();
-		client1.setId(1L);
-		client1.setName("Joao das Couves");
-		client1.setEmail("joaodascouves@email.com");
-		client1.setTelephone("99988-7777");
-		
-		var client2 = new Client();
-		client2.setId(2L);
-		client2.setName("Maria da Silva");
-		client2.setEmail("mariadasilva@email.com");
-		client2.setTelephone("99977-6666");
-		
-		return Arrays.asList(client1, client2);
+		return manager.createQuery("from Client", Client.class).getResultList();
 	}
 	
 }
--- src/main/java/com/algaworks/algalog/domain/model/Client.java
+++ src/main/java/com/algaworks/algalog/domain/model/Client.java
@@ -1,15 +1,29 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Client {
 
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
+	
+	@Column(name = "phone")
 	private String telephone;
 	
 }
